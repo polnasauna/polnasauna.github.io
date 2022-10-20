030238c8e9813179cfc0b70674ca4db67986270f
Polna Sauna <polnasauna@github.com>
2022-10-20 21:13:42 +0200

    Click on inactive to move month

--- calendar.js
+++ calendar.js
@@ -7,6 +7,50 @@
     this.current = moment().date(1);
     this.slotCallback = slotCallback;
     this.draw();
+    this.addSwipeListener();
+  }
+
+  Calendar.prototype.addSwipeListener = function () {
+    var self = this;
+    this.el.addEventListener("touchstart", startTouch, false);
+    this.el.addEventListener("touchmove", moveTouch, false);
+
+    // Swipe Up / Down / Left / Right
+    var initialX = null;
+    var initialY = null;
+
+    function startTouch(e) {
+      initialX = e.touches[0].clientX;
+      initialY = e.touches[0].clientY;
+    };
+
+    function moveTouch(e) {
+      if (initialX === null)
+        return;
+
+      if (initialY === null)
+        return;
+
+      var currentX = e.touches[0].clientX;
+      var currentY = e.touches[0].clientY;
+
+      var diffX = initialX - currentX;
+      var diffY = initialY - currentY;
+
+      if (Math.abs(diffX) > Math.abs(diffY)) {
+        // sliding horizontally
+        if (diffX > 0) {
+          self.nextMonth();
+        } else {
+          self.prevMonth();
+        }
+      }
+
+      initialX = null;
+      initialY = null;
+
+      e.preventDefault();
+    };
   }
 
   Calendar.prototype.fetchAvailableSlots = function () {
