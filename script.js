5584e7a28eef698224c947b1131977ef4720ab96
Polna Sauna <polnasauna@github.com>
2023-01-24 08:07:36 +0100

    fix error on success page

--- script.js
+++ script.js
@@ -5,13 +5,15 @@
 
 const toTop = document.querySelector(".to-top");
 
-window.addEventListener("scroll", () => {
-  if (window.pageYOffset > 100) {
-    toTop.classList.add("active");
-  } else {
-    toTop.classList.remove("active");
-  }
-});
+if (toTop) {
+  window.addEventListener("scroll", () => {
+    if (window.pageYOffset > 100) {
+      toTop.classList.add("active");
+    } else {
+      toTop.classList.remove("active");
+    }
+  });
+}
 
 // window.addEventListener("load", () => {
 //   document.getElementById("bannerMessage").innerHTML = bannerMessage;
